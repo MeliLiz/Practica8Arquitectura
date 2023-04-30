59a5a626971c5b8284d6a667d25392d68c0ebefa
MeliLiz <melifernandez@ciencias.unam.mx>
2023-04-30 17:46:35 -0600

    En cat que se separan las cadenas de los argumentos

--- P8.asm
+++ P8.asm
@@ -72,7 +72,9 @@ main:
 	
 	#Caso cat
 	la $s3, cat	#Cargamos a $s3 la direccion de cat
-	jal compara
+	li $t8, 0 #Contador de bytes
+	li $t7, 2 #Numero de bytes que ocupa la palabra cat
+	jal compara1
 	beqz $t9, caso_cat
 	
 	#Caso exit
@@ -230,10 +232,60 @@ caso_rev:
 	j main
 	
 caso_cat:
-	la $a0, cat
+	la $s2, buffer
+	addi $s2, $s2, 4 #Nos sqaltamos a la direccion de memoria en la que empieza el primer argumento
+	
+	li $t8, 0 	   #Contador del numero de bytes que usa la palabra 1
+	li $t7, 0
+	li $t6, 0x10010000 #inicializamos $t6
+	
+loop:#Contar el numero de bytes que usa la primera cadena y copiarla a $t6
+	
+	lb $t0, 0($s2)#Cargamos el siguiente byte de la cadena en $t0
+	
+	beq $t0, $zero, fin #Si el byte es cero, terminamos
+	beq $t0, 0x20, copiar #Si el byte es espacio, ya llegamos al separador
+	
+	sb $t0, ($t6)  #Guardamos el siguiente byte de la cadena (aqui vamos poniendo la palabra 1) en el siguiente byte de $t6
+	
+	addi $t6, $t6, 1 #Vamos al siguiente byte de $t6 (primera subcadena)
+	addi $t8, $t8, 1 #Aumentamos el contador
+	addi $s2, $s2, 1 #Vamos al siguiente byte de $s0 (cadena)
+	j loop
+	
+copiar: #Copiar la segunda cadena en $t7
+
+	sb $zero, ($t6)#agregamos el caracter nulo para que ahi se corte t6 (primera subcadena)
+	
+	sub $t7, $s2, $t8 #volver a la direccion donde empieza la cadena (en $t7 guardaremos la segunda subcadena)
+	
+	li $v0,4
+	
+	addi $t8, $t8, 1 #sumarle el byte del espacio al contador
+	add $t7, $t7,$t8 #sumarle los bytes de la primera palabra (para que $t7 empiece en la dirección de memoria donde empieza la segunda palabra)
+	
+	move $a0, $t7	#obtenemos la segunda palabra
+	syscall
+	
+	subi $t8, $t8, 1
+	la $s2, buffer  #Volovemos a poner la direccion de la cadena en $s0
+	addi $s2, $s2, 4
+	sub $t6, $t6, $t8 #Vamos a la direccion de memoria donde comienza la primera palabra
+	
+	move $a0, $t6
+	syscall
+	
+	#Ahora la primera subcadena esta en $t6 y la segunda en $t7
+	j main
+	
+	
+fin:
+	la $a0, cat_func
 	li $v0, 4
 	syscall
 	j main
+	
+	
 	
 caso_exit:
 	la $a0, exit
