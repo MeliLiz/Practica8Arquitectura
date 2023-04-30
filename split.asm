0a3f21bbfb5f793e0e51d4ce92330c45287bcf6f
MeliLiz <melifernandez@ciencias.unam.mx>
2023-04-30 13:50:19 -0600

    Corta el caracter nulo, ya se hacen las dos cadenas separadas

--- split.asm
+++ split.asm
@@ -17,16 +17,40 @@ loop:
 	j loop
 	
 copiar:
-	sub $t7, $s0, $t8
+	sub $t7, $s0, $t8 #colver a la direccion donde empieza la cadea
 	li $v0,4
-	#move $a0, $t7
-	#syscall
-	addi $t7, $t7, 1
-	add $t7, $t7,$t8
-	move $a0, $t7
+	
+	addi $t8, $t8, 1 #sumarle el byte del espacio al contador
+	add $t7, $t7,$t8 #sumarle los bytes de la primera palabra
+	move $a0, $t7	#obtenemos la segunda palabra
 	syscall
+	
+	subi $t8, $t8, 1
+	la $s0, cadena  #Volovemos a poner la direccion de la cadena en $s0
+	li $t6, 0x10010000
+	
+loop1:
+	lb $t0, ($s0)
+	beq $t0, $zero, error
+	beq $t0, 0x20, copy
+	
+	sb $t0, ($t6)
+	
+	addi $t6, $t6, 1
+	addi $s0, $s0, 1
+	j loop1
+	
+copy:
+	#addi $t6, $t6, 1
+	sb $zero, ($t6)#agregamos el caracter nulo para que ahi se corte t6
+	sub $t6, $t6, $t8
+	move $a0, $t6
+	syscall
+	
 	
 	
 fin:
 	li $v0, 10
 	syscall
+	
+error:
